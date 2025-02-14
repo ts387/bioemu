359a86f1b73fbcfc1cee86e2804901e233944437
José Jiménez-Luna <hawk31@gmail.com>
2025-02-14 09:46:26 +0000

    chore: setup changes for notebooks (#21)
    Introduces 2 changes:
    
    * `setup.sh` will optionally read 2 environment variables if set.
    ${BIOEMU_ENV_NAME}, which is the name conda environment bioemu will be
    installed in, and ${UPDATE_ENV}, to force update of an existing
    environment. The latter argument is introduced because in Colab
    notebooks, only the `base` environment is available to the user via
    condacolab, and switching to a different environment (jupyter kernel)
    from within the notebook is not possible.
    * `environment.yml` now lists Python>3.10 as a dep instead of pinning it
    down to 3.12. It's possible that changing the channel priority
    restriction argument might also fix the resolution error.

--- setup.sh
+++ setup.sh
@@ -2,7 +2,8 @@
 set -ex
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-BIOEMU_ENV_NAME=bioemu
+BIOEMU_ENV_NAME="bioemu"
+UPDATE_ENV="${UPDATE_ENV:-0}"
 
 # Set up colabfold
 export COLABFOLD_DIR=$HOME/.localcolabfold # Where colabfold will be installed
@@ -13,16 +14,27 @@ else
 fi
 
 # Create conda env. You may be able to skip the conda steps if zlib and python>=3.10 are already installed.
+CURRENT_ENV_NAME=$(basename ${CONDA_PREFIX})
 CONDA_PREFIX=$(conda info --base)
-if [ -d $CONDA_PREFIX/envs/$BIOEMU_ENV_NAME ]; then
-  echo "BioEmu env already exists"
-else
-  conda env create -f $SCRIPT_DIR/environment.yml -n $BIOEMU_ENV_NAME
+
+if [ $UPDATE_ENV -eq 1 ]; then # Force update of current environment (to install in base env on notebooks like Colab)
+  conda env update --name ${CURRENT_ENV_NAME} --file ${SCRIPT_DIR}/environment.yml --prune
+else # try install from scratch
+  if [ -d $CONDA_PREFIX/envs/$BIOEMU_ENV_NAME ]; then
+    echo "${BIOEMU_ENV_NAME} env already exists"
+  else
+    conda env create -f $SCRIPT_DIR/environment.yml -n $BIOEMU_ENV_NAME
+  fi
 fi
 
 # Make bash aware of conda
 eval "$(conda shell.bash hook)"
-conda activate $BIOEMU_ENV_NAME
+
+if [ $UPDATE_ENV -eq 0 ]; then
+  conda activate $BIOEMU_ENV_NAME
+else
+  conda activate $CURRENT_ENV_NAME
+fi
 
 # Install bioemu in the new conda env.
 uv pip install -e $SCRIPT_DIR
